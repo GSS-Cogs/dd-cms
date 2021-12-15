cd3142babddad2aaf811296a9bb41b9c81184ac2
Alex Tucker <alex@floop.org.uk>
2021-12-15 16:34:27 +0000

    Bootstrap a local dev environment for IntelliJ/PyCharm from the Docker plone:5 image.

---
+++ plone-5/bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+mkdir -p instance
+plone=$(docker create plone:5)
+for f in base.cfg buildout-base.cfg buildout.cfg develop.cfg lxml_static.cfg requirements.txt
+do
+  docker cp $plone:/plone/instance/$f instance/$f
+done
+docker rm -v $plone
+
+sed -i 's|var-dir=/data|var-dir=data|;/RelStorage\|psycopg2\|mysqlclient\|cx-Oracle/d' instance/buildout.cfg
+
+pipenv --python 3.8
+
+# Grab the ENV variables from the Plone 5 docker image
+# N.B. Sets versions used below and also PATH and LANG
+export $(docker inspect --format='{{join .Config.Env " "}}' plone:5)
+
+pipenv install pip==$PIP setuptools==$SETUPTOOLS zc.buildout==$ZC_BUILDOUT wheel==$WHEEL
+cd instance
+pipenv run buildout
