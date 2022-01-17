a3d54dc52066da4ab839e6a0c75901529dddf1a2
Steven Dickinson <steven.dickinson@methods.co.uk>
2022-01-17 10:58:25 +0000

    Use styled-components to bring some formatting to topic and title in a tile

--- src/addons/volto-climatechange-elements/src/GssCogsElements/DashboardTileView.jsx
+++ src/addons/volto-climatechange-elements/src/GssCogsElements/DashboardTileView.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
+import { spacing, typography } from '@govuk-react/lib';
+import { PANEL_TEXT } from 'govuk-colours';
+import { DASHBOARD_TILE_BACKGROUND, COLOR_SECONDARY, COLOR_PRIMARY } from '../colors';
+
+
+const DashboardTileContainer = styled('div')(
+    {
+        background: `${DASHBOARD_TILE_BACKGROUND}`,
+        color: `${PANEL_TEXT}`,
+    },
+    typography.font({ size: 16 }),
+    spacing.withWhiteSpace({
+        padding: [
+            { size: 3, },
+        ],
+    }),
+    {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+);
+
+const DashboardTileTopic = styled('div')(
+    {
+        color: COLOR_SECONDARY,
+        ...typography.font({ size: 16, weight: 400. }),
+    },
+    spacing.withWhiteSpace({
+        padding: [
+            { size: 0, },
+        ],
+    }),
+);
+
+const DashboardTileTitle = styled('div')(
+    {
+        color: COLOR_PRIMARY,
+        ...typography.font({ size: 19, weight: 'bold' }),
+    },
+    spacing.withWhiteSpace({
+        padding: [
+            { size: 1, direction: 'top' },
+        ],
+    }),
+);
+
 
 export const DashboardTileView = ({ data }) => {
     return (
-        <div>
-            {data.topic}<br/>
-            {data.title}<br/>
-        </div>
+        <DashboardTileContainer>
+            <DashboardTileTopic>{data.topic}</DashboardTileTopic>
+            <DashboardTileTitle>{data.title}</DashboardTileTitle>
+        </DashboardTileContainer>
     );
 };
---
+++ src/addons/volto-climatechange-elements/src/colors.js
@@ -0,0 +1,4 @@
+export const DASHBOARD_TILE_BACKGROUND = '#F3F2F1';
+
+export const COLOR_PRIMARY = '#0B0C0C';
+export const COLOR_SECONDARY = '#505A5F';
