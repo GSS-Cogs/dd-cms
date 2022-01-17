a2f287c6b93b8c6679436de1f065c0d56106dc63
Steven Dickinson <steven.dickinson@methods.co.uk>
2022-01-17 13:43:32 +0000

    Basics of using a schema to produce volto form

--- src/addons/volto-climatechange-elements/src/GssCogsElements/DashboardTile/DashboardTileEdit.jsx
+++ src/addons/volto-climatechange-elements/src/GssCogsElements/DashboardTile/DashboardTileEdit.jsx
@@ -1,39 +1,28 @@
 import React from 'react';
 import { DashboardTileView } from './DashboardTileView';
-import { Segment, Form } from 'semantic-ui-react';
-import { SidebarPortal, Field } from '@plone/volto/components';
+import { Segment } from 'semantic-ui-react';
+import { SidebarPortal, InlineForm } from '@plone/volto/components';
+import { DashboardTileSchema } from './schema';
 
 export const DashboardTileEdit = (props) => {
     const { selected, onChangeBlock, block, data } = props;
+    const schema = DashboardTileSchema(props);
 
     return (
         <div>
             <SidebarPortal selected={selected}>
                 <Segment.Group raised>
-                    <header className="header pulled">
-                        <h2>Data table</h2>
-                    </header>
-
-                    <Form>
-                        <Field
-                            id="topic"
-                            widget="string"
-                            title="Topic"
-                            value={data.topic || ''}
-                            onChange={(id, value) =>
-                                onChangeBlock(block, { ...data, [id]: value })
-                            }
-                        />
-                        <Field
-                            id="title"
-                            widget="string"
-                            title="title"
-                            value={data.title || ''}
-                            onChange={(id, value) =>
-                                onChangeBlock(block, { ...data, [id]: value })
-                            }
-                        />
-                    </Form>
+                    <InlineForm
+                        schema={schema}
+                        title={schema.title}
+                        onChangeField={(id, value) => {
+                            onChangeBlock(block, {
+                                ...data,
+                                [id]: value,
+                            });
+                        }}
+                        formData={data}
+                    />
                 </Segment.Group>
             </SidebarPortal>
 
