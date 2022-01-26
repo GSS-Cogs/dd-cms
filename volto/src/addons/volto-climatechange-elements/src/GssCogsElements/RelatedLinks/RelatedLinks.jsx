2ccffe3a26c5d0921ea15c2e3f86179acca4fe51
Steven Dickinson <steven.dickinson@methods.co.uk>
2022-01-26 15:48:51 +0000

    Styling tweaks, some more related links

--- volto/src/addons/volto-climatechange-elements/src/GssCogsElements/RelatedLinks/RelatedLinks.jsx
+++ volto/src/addons/volto-climatechange-elements/src/GssCogsElements/RelatedLinks/RelatedLinks.jsx
@@ -3,29 +3,43 @@ import { H3, H4 } from 'govuk-react';
 
 import './RelatedLinks.scss';
 
+const TEST_ITEMS = [
+  {
+    title: 'PMD - Publish My Data',
+    content:
+      'IDS is bringing together data from across the UK government and ' +
+      'Devolved Administrations, with the Data Explorer providing new ways to ' +
+      'search across and filter datasets to give you the data you need.',
+  },
+  {
+    title: 'Met Office',
+    content:
+      'Met Office weather forecasts for the UK. World leading weather services for the public.',
+  },
+  {
+    title: 'Forestry Research',
+    content:
+      "Forest Research is Great Britain's principal organisation for forestry and tree related research and is internationally renowned for the provision of evidence...",
+  },
+  {
+    title: 'Department for Business, Energy & Industrial Strategy',
+    content:
+      'Building a stronger, greener future by fighting coronavirus, tackling climate change, unleashing innovation and making the UK a great place to work and do business...',
+  },
+];
+
 export const RelatedLinks = () => (
   <div className="cc-related-links">
     <H3>Related Links</H3>
     <ul className="cc-related-links--list">
-      <li className="cc-related-links--item">
-        <H4>
-          <a href="#">PMD - Publish My Data</a>
-        </H4>
-        <div>
-          IDS is bringing together data from across the UK government and
-          Devolved Administrations, with the Data Explorer providing new ways to
-          search across and filter datasets to give you the data you need.
-        </div>
-      </li>
-      <li className="cc-related-links--item">
-        <H4>
-          <a href="#">Met Office</a>
-        </H4>
-        <div>
-          Met Office weather forecasts for the UK. World leading weather
-          services for the public.
-        </div>
-      </li>
+      {TEST_ITEMS.map((data, i) => (
+        <li key={i} className="cc-related-links--item">
+          <H4>
+            <a href="#">{data.title}</a>
+          </H4>
+          <div>{data.content}</div>
+        </li>
+      ))}
     </ul>
   </div>
 );
