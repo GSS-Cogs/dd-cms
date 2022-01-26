af80931f094682cfec0a3017b4de7c8bde18238c
Lewis Coulson <lewiscoulson@Lewiss-MacBook-Pro.local>
2022-01-26 13:44:39 +0000

    Use external theme for govuk styling

--- volto/src/theme.js
+++ volto/src/theme.js
@@ -1,4 +1,2 @@
 import 'semantic-ui-less/semantic.less';
 import '@plone/volto/../theme/themes/pastanaga/extras/extras.less';
-import 'govuk-frontend/govuk/all.scss';
-import './styles/custom.scss';
