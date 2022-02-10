bc54ebe2462b46d00f7b50c53b6982f61d5ea462
Steven Dickinson <sd@tonic.works>
2022-02-10 15:31:01 +0000

    Real basics of trying to include the chart builder as a block type in volto

--- volto/src/addons/volto-chart-builder/src/index.js
+++ volto/src/addons/volto-chart-builder/src/index.js
@@ -1,4 +1,24 @@
+import imageSVG from '@plone/volto/icons/image.svg';
+import App from 'chart-builder/src/App.tsx';
+
 const applyConfig = (config) => {
+
+  config.blocks.blocksConfig.chartBuilder = {
+    id: 'chartBuilder',
+    title: 'Chart Builder',
+    icon: imageSVG,
+    group: 'common',
+    view: App,
+    edit: App,
+    restricted: false,
+    mostUsed: true,
+    sidebarTab: 1,
+    security: {
+      addPermission: [],
+      view: [],
+    },
+  };
+
   return config;
 };
 
