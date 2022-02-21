3896c2af5835673e35adca8b11e4574649ad0063
Steven Dickinson <steven.dickinson@methods.co.uk>
2022-02-21 15:52:58 +0000

    Fix preview mode problems by wrapping the chart builder edit-view in another component

--- volto/src/addons/volto-chart-builder/src/components/ChartBuilderEdit.jsx
+++ volto/src/addons/volto-chart-builder/src/components/ChartBuilderEdit.jsx
@@ -6,19 +6,22 @@ import ChartPanel from 'chart-builder/src/components/chart-panel/ChartPanel';
 import ChartContext from 'chart-builder/src/context/ChartContext';
 import CSVUploader from 'chart-builder/src/components/side-panel/csv-uploader/CSVUploader';
 
-export const ChartBuilderEdit = ({ selected }) => {
+const View = () => {
   const { previewMode } = useContext(ChartContext);
-  
+
+  return previewMode ? <ChartPanel /> : <CSVUploader />;
+};
+
+export const ChartBuilderEdit = ({ selected }) => {
   return (
     <ChartContextProvider>
-        <SidebarPortal selected={selected}>
-          <div id="chart-builder">
-            <SidePanel />
-          </div>
-        </SidebarPortal>
+      <SidebarPortal selected={selected}>
+        <div id="chart-builder">
+          <SidePanel />
+        </div>
+      </SidebarPortal>
 
-
-        {previewMode ? <ChartPanel /> : <CSVUploader />}
+      <View />
     </ChartContextProvider>
   );
 };
