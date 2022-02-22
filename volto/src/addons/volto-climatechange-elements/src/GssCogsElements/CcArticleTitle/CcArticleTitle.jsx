aedaf445c2e63e21a3b9cfd86268a043f4efdd9b
Lewis Coulson <lewiscoulson@Lewiss-MacBook-Pro.local>
2022-02-22 15:23:41 +0000

    Add dynamic and static content switch

---
+++ volto/src/addons/volto-climatechange-elements/src/GssCogsElements/CcArticleTitle/CcArticleTitle.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const CcArticleTitle = ({ title }) => {
+    return <h1>{title}</h1>
+};
--- volto/src/addons/volto-climatechange-elements/src/GssCogsElements/CcV2Preview/CcV2ArticleView.jsx
+++ volto/src/addons/volto-climatechange-elements/src/GssCogsElements/CcV2Preview/CcV2ArticleView.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 import { CcArticleHeader } from '../CcArticleHeader/CcArticleHeader';
 import { Paragraph } from 'govuk-react';
 
-export const CcV2ArticleView = () => (
-  <div>
+export const CcV2ArticleView = ({content}) => {
+  const isDynamic = false;
+  const getContent = (staticContent, dynamicKey) => isDynamic ? dynamicKey : staticContent;
+
+  const data = {
+    title: getContent('Measuring greenhouse gas emissions', content.title),
+    summary: getContent('The UK is required to report its estimated greenhouse gas (GHG) emissions on a range of different bases to fulfil a wide range of international agreements as well as for domestic policy making purposes.', content.summary),
+    href: '#',
+    linkTitle: 'All climate and weather data',
+  }
+
+  return (<div>
     <CcArticleHeader
-      data={{
-        title: 'Measuring greenhouse gas emissions',
-        summary: 'The UK is required to report its estimated greenhouse gas (GHG) emissions on a range of different bases to fulfil a wide range of international agreements as well as for domestic policy making purposes.',
-        href: '#',
-        linkTitle: 'All climate and weather data',
-      }}
+      data={data}
     />
     <div className="govuk-width-container">
       <Paragraph>
-        The report found unequivocal evidence that observed warming of the climate is a consequence of emissions from human activity that has increased the concentration of greenhouse gases in the global atmosphere. Human induced climate change has already affected the severity and frequency of many types of extreme weather and climate events.
+        { getContent('The report found unequivocal evidence that observed warming of the climate is a consequence of emissions from human activity that has increased the concentration of greenhouse gases in the global atmosphere. Human induced climate change has already affected the severity and frequency of many types of extreme weather and climate events.', content.body) }
       </Paragraph>
     </div>
-  </div>
-);
+  </div>)
+};
