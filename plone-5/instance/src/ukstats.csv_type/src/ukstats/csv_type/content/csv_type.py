621de5c4d7f60ef51bdd3c3491236ba964d7c6fa
Anoj Perera <anojmperera@gmail.com>
2022-03-04 11:37:23 +0000

    Added content to csv

---
+++ plone-5/instance/src/ukstats.csv_type/src/ukstats/csv_type/content/csv_type.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from plone.dexterity.content import Container
+from plone.supermodel import model
+
+
+class IArticleType(model.Schema):
+    """ Marker interface for CSV Type
+    """
