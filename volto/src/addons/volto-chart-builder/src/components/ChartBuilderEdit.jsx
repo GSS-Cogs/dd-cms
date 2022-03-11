97eb338d54c3272c68326a0e8064b3957cdbf222
Steven Dickinson <steven.dickinson@methods.co.uk>
2022-03-11 14:40:59 +0000

    Amend import after slight tweaking of chart-builder filename

--- volto/src/addons/volto-chart-builder/src/components/ChartBuilderEdit.jsx
+++ volto/src/addons/volto-chart-builder/src/components/ChartBuilderEdit.jsx
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { Field, SidebarPortal } from '@plone/volto/components';
 import { Form, Segment } from 'semantic-ui-react';
 import { useChartContext } from 'chart-builder/src/context/ChartContextProvider';
-import initialChartState from 'chart-builder/src/context/initialChartState';
+import initialChartProperties from 'chart-builder/src/context/initialChartProperties';
 import SidePanel from 'chart-builder/src/components/side-panel/SidePanel';
 import ChartPreview from 'chart-builder/src/components/chart-panel/chart-preview/ChartPreview';
 import ChartContext from 'chart-builder/src/context/ChartContext';
@@ -79,7 +79,7 @@ export function useBlockChartContextState(props) {
   const [chartProperties, setChartProperties] = useVoltoBlockDataState(
     data,
     'chartProperties',
-    initialChartState,
+    initialChartProperties,
   );
   const [selectedFilename, setSelectedFilename] = useVoltoBlockDataState(
     data,
