9ec566aa8e36b3e65b8cfc78120a7315a78f278f
Steven Dickinson <steven.dickinson@methods.co.uk>
2022-03-11 15:03:29 +0000

    Rename "validateData" to slightly more semantic "importCsvData"

--- volto/src/addons/volto-chart-builder/src/hooks/index.js
+++ volto/src/addons/volto-chart-builder/src/hooks/index.js
@@ -4,7 +4,7 @@ import ChartContext from 'chart-builder/src/context/ChartContext';
 import { getCsvData } from '../actions';
 
 export function usePloneCsvData(plone_ref) {
-  const { validateData } = useContext(ChartContext);
+  const { importCsvData } = useContext(ChartContext);
   const dispatch = useDispatch();
 
   const file = plone_ref.length ? plone_ref[0] : null;
@@ -30,7 +30,7 @@ export function usePloneCsvData(plone_ref) {
       fileData.loaded &&
       file['@type'] === 'File'
     ) {
-      validateData(fileData.content, file['@id']);
+      importCsvData(fileData.content, file['@id']);
     }
-  }, [fileData, file, validateData]);
+  }, [fileData, file, importCsvData]);
 }
