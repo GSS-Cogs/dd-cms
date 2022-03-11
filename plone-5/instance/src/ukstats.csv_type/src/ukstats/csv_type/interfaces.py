f4bc68a4b9ece90ceb1be74da65c14c671fe193e
Anoj Perera <anojmperera@gmail.com>
2022-03-11 09:30:39 +0000

    Interface added to load json

--- plone-5/instance/src/ukstats.csv_type/src/ukstats/csv_type/interfaces.py
+++ plone-5/instance/src/ukstats.csv_type/src/ukstats/csv_type/interfaces.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from example.conference import _
 from plone.app.textfield import RichText
 from plone.supermodel import model
 from zope import schema
@@ -11,44 +10,3 @@ from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 class IUkstatsCsvTypeLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
-
-
-class ICSV(model.Schema):
-
-    """A conference program. Programs can contain Sessions."""
-
-    aboutUrl = schema.URI(
-        title=_(u'About URL'),
-        description=_(u'About URL'),
-        required=False,
-    )
-
-    base = schema.Text(
-        title=_(u'CSV File Name'),
-        description=_(u'CSV File Name'),
-        required=True
-    )
-
-    csv_stream = schema.Text(
-        title=_(u'Stream'),
-        description=_(u'CSV Stream'),
-        required=False
-    )
-
-    datatype = schema.Text(
-        title=_(u'Data Type'),
-        description=_(u'Data Type'),
-        required=False,
-    )
-
-    metadata_filename = schema.Text(
-        title=_(u'File Name'),
-        description=_(u'File Name'),
-        required=False,
-    )
-
-    containing_graph_uri = schema.URI(
-        title=_(u'Graph URI'),
-        description=_(u'URI'),
-        required=False,
-    )
