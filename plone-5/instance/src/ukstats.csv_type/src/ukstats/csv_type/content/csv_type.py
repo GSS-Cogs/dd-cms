109577ec0a2cd2009c3cadbfa2a52144971641de
Steven Dickinson <steven.dickinson@methods.co.uk>
2022-03-15 16:50:05 +0000

    Create a marker interface for CSVW content type, and use that to provide the @connector-data adapter
    - with static data for now...

--- plone-5/instance/src/ukstats.csv_type/src/ukstats/csv_type/content/csv_type.py
+++ plone-5/instance/src/ukstats.csv_type/src/ukstats/csv_type/content/csv_type.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from plone.dexterity.content import Container
 from plone.supermodel import model
-
+from plone.dexterity.content import Container
 
 class ICSVType(model.Schema):
     """ Marker interface for CSV Type
     """
+
+
+class ICSVMarker(Container):
+    """ Another marker """
