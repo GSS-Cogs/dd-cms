a5c2f98a9e79ef0a8b0c517078fed1f97fea4869
Steven Dickinson <steven.dickinson@methods.co.uk>
2022-03-15 16:50:05 +0000

    Create a marker interface for CSVW content type, and use that to provide the @connector-data adapter
    - with static data for now...

---
+++ plone-5/instance/src/ukstats.csv_type/src/ukstats/csv_type/adapter.py
@@ -0,0 +1,38 @@
+""" behavior module """
+
+import logging
+
+from plone.memoize import ram
+from zope.component import adapter
+from zope.interface import implementer
+from zope.publisher.interfaces.browser import IBrowserRequest
+
+from eea.api.dataconnector.interfaces import IDataProvider
+from ukstats.csv_type.content.csv_type import ICSVMarker
+
+logger = logging.getLogger(__name__)
+
+
+@adapter(ICSVMarker, IBrowserRequest)
+@implementer(IDataProvider)
+class DataProviderForCsvws(object):
+    """ data provider for connectors """
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    # TO DO: persistent caching, periodical refresh, etc
+    @ram.cache(lambda func, self: (self.context.modified(), self.request.form))
+    def _provided_data(self):
+        """ provided data """
+        return {
+            "results": {
+                "a CSV w derived field": True
+            }
+        }
+
+    @property
+    def provided_data(self):
+        """ provided data """
+        return self._provided_data()
---
+++ plone-5/instance/src/ukstats.csv_type/src/ukstats/csv_type/content/csv_type.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from plone.dexterity.content import Container
+
+
+class ICSVMarker(Container):
+    """ Another marker """
