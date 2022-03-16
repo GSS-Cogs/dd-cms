983edb7de36f3dfcdcf40c66d7df82f9b7efd1d6
Anoj Perera <anojmperera@gmail.com>
2022-03-16 23:04:51 +0000

    Removed duplicated fields

--- plone-5/instance/src/ukstats.csv_type/src/ukstats/csv_type/csv.py
+++ plone-5/instance/src/ukstats.csv_type/src/ukstats/csv_type/csv.py
@@ -5,25 +5,9 @@ from zope import schema
 
 
 class ICSV(model.Schema):
-
     """A conference program. Programs can contain Sessions."""
-
-    title = schema.TextLine(
-        title=_(u'CSVW Title'),
-        required=True,
-    )
-
-    description = schema.Text(
-        title=_(u'CSVW Description'),
-    )
-
-
     csv = NamedBlobFile(
         title=_(u'csv'),
-        required=True,
-    )
-
-    json = NamedBlobFile(
-        title=_(u'JSON Meta'),
+        description=_(u'Upload your csv file'),
         required=True,
     )
