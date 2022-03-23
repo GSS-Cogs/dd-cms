07cb40400268fd1b95a462b5c28453e96ffbe072
Steven Dickinson <steven.dickinson@methods.co.uk>
2022-03-23 15:06:21 +0000

    Minor doc tweaks

--- plone-5/instance/src/ukstats.csv_type/src/ukstats/csv_type/content/csv_type.py
+++ plone-5/instance/src/ukstats.csv_type/src/ukstats/csv_type/content/csv_type.py
@@ -3,4 +3,8 @@ from plone.dexterity.content import Container
 
 
 class ICSVMarker(Container):
-    """ Another marker """
+    """
+    Marker interface that defines the content type,
+    and allows us to target an adapter to produce
+    @connector-data compatible output.
+    """
