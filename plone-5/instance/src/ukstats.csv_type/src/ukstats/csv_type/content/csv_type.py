d1149f98dafa6e7e0106bda88c4b0d1014a8d5ba
Anoj Perera <anojmperera@gmail.com>
2022-03-23 10:12:44 +0000

    Delete model document

--- plone-5/instance/src/ukstats.csv_type/src/ukstats/csv_type/content/csv_type.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from plone.dexterity.content import Container
-from plone.supermodel import model
-
-
-class ICSVType(model.Schema):
-    """ Marker interface for CSV Type
-    """
