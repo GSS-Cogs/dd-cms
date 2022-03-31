a6366d90f475bb2184858e5ded1ea2480db72a1c
Lewis Coulson <lewiscoulson@Lewiss-MacBook-Pro.local>
2022-03-31 13:41:30 +0100

    Start editing right hand side widgets styling

---
+++ volto/src/addons/volto-govuk-theme/src/customizations/volto/components/manage/Widgets/TextareaWidget.jsx
@@ -0,0 +1,95 @@
+/**
+ * TextareaWidget component.
+ * @module components/manage/Widgets/TextareaWidget
+ */
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Label, TextArea } from 'semantic-ui-react';
+
+import { injectIntl } from 'react-intl';
+import { FormFieldWrapper } from '@plone/volto/components';
+
+/**
+ * TextareaWidget component class.
+ * @function TextareaWidget
+ * @returns {string} Markup of the component.
+ */
+const TextareaWidget = (props) => {
+  const { id, maxLength, value, onChange, placeholder } = props;
+  const [lengthError, setlengthError] = useState('');
+
+  const onhandleChange = (id, value) => {
+    if (maxLength & value?.length) {
+      let remlength = maxLength - value.length;
+      if (remlength < 0) {
+        setlengthError(`You have exceed word limit by ${Math.abs(remlength)}`);
+      } else {
+        setlengthError('');
+      }
+    }
+    onChange(id, value);
+  };
+
+  return (
+    <div className="govuk-form-group">
+      <label className="govuk-label" htmlFor={`field-${id}`}>
+        {id}
+      </label>
+      <textarea
+        id={`field-${id}`}
+        className="govuk-textarea"
+        name={id}
+        value={value || ''}
+        disabled={props.isDisabled}
+        placeholder={placeholder}
+        onChange={({ target }) =>
+          onhandleChange(id, target.value === '' ? undefined : target.value)
+        }
+      />
+      {lengthError.length > 0 && (
+        <Label key={lengthError} basic color="red" pointing>
+          {lengthError}
+        </Label>
+      )}
+    </div>
+  );
+};
+
+/**
+ * Property types.
+ * @property {Object} propTypes Property types.
+ * @static
+ */
+TextareaWidget.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  maxLength: PropTypes.number,
+  required: PropTypes.bool,
+  error: PropTypes.arrayOf(PropTypes.string),
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  onEdit: PropTypes.func,
+  onDelete: PropTypes.func,
+  wrapped: PropTypes.bool,
+  placeholder: PropTypes.string,
+};
+
+/**
+ * Default properties.
+ * @property {Object} defaultProps Default properties.
+ * @static
+ */
+TextareaWidget.defaultProps = {
+  description: null,
+  maxLength: null,
+  required: false,
+  error: [],
+  value: null,
+  onChange: null,
+  onEdit: null,
+  onDelete: null,
+};
+
+export default injectIntl(TextareaWidget);
