b14ffbbe58f74c31d0f7f2dca89cc635f15b030c
Anoj Perera <anojmperera@gmail.com>
2022-04-11 08:39:54 +0100

    CSV View and namespace added

--- plone-5/instance/src/ukstats.csv_type/src/ukstats/csv_type/csv.py
+++ plone-5/instance/src/ukstats.csv_type/src/ukstats/csv_type/csv.py
@@ -1,6 +1,9 @@
 """CSV Content Type Schema."""
+from plone import api
 from plone.namedfile.field import NamedBlobFile
 from plone.supermodel import model
+from Products.Five import BrowserView
+
 from ukstats.csv_type import _
 
 
@@ -16,5 +19,16 @@ class ICSV(model.Schema):
     json = NamedBlobFile(
         title=_("JSON Metadata"),
         description=_("Upload your JSON Metadata file"),
-        required=True,
+        required=False,
     )
+
+
+class CSVView(BrowserView):
+    def csv(self):
+        context = aq_inner(self.context)
+        catalog = api.portal.get_tool(name="portal_catalog")
+
+        return catalog(
+            object_provides=ICSV.__identifier__,
+            path="/".join(context.getPhysicalPath()),
+        )
