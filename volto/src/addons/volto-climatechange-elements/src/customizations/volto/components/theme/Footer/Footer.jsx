edceafb3c443dc5ca6c1d861b82e93b50585262b
Anoj Perera <anojmperera@gmail.com>
2022-05-06 09:27:01 +0100

    Logout link added to footer

---
+++ volto/src/addons/volto-climatechange-elements/src/customizations/volto/components/theme/Footer/Footer.jsx
@@ -0,0 +1,99 @@
+/**
+ * Footer component.
+ * @module components/theme/Footer/Footer
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import config from '@plone/volto/registry';
+import { Footer as GovukFooter } from 'govuk-react-jsx';
+
+/**
+ * Component to display the footer.
+ * @class Footer
+ * @extends Component
+ */
+export class Footer extends Component {
+
+  /**
+   * Property types.
+   * @property {Object} propTypes Property types.
+   * @static
+   */
+  static propTypes = {
+    token: PropTypes.string,
+    content: PropTypes.shape({
+      '@id': PropTypes.string,
+    }),
+  };
+
+
+  /**
+   * Default properties.
+   * @property {Object} defaultProps Default properties.
+   * @static
+   */
+  static defaultProps = {
+    token: null,
+    content: {
+      '@id': null,
+    },
+  };
+
+  render() {
+    const { settings } = config;
+    let items = [];
+    const returnUrl = this.props.content
+      ? `?return_url=${this.props.content['@id'].replace(
+        settings.apiPath,
+        '',
+      )}`
+      : ''
+    if (this.props.token) {
+      items =
+        [{
+          children: 'Log Out',
+          href: `/logout${returnUrl}`
+        },
+        {
+          children: 'Item 2',
+          href: '/footer-meta-item-2'
+        },
+        {
+          children: 'Item 3',
+          href: '/'
+        }
+        ];
+    } else {
+      items =
+        [{
+          children: 'Item 1',
+          href: '/'
+        },
+        {
+          children: 'Item 2',
+          href: '/footer-meta-item-2'
+        },
+        {
+          children: 'Item 3',
+          href: '/'
+        }
+        ];
+    }
+    return (
+      <GovukFooter
+        containerClassName="volto-width-container--wide"
+        meta={{
+          items,
+          visuallyHiddenTitle: 'Support links'
+        }}
+      />
+    );
+  }
+}
+
+export default connect((state) => ({
+  token: state.userSession.token,
+  content: state.content.data,
+}))(Footer);
