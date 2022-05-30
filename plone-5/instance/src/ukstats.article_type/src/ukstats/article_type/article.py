8283aacffedc7bdad562b82b72972e45a503d2d6
Anoj Perera <anojmperera@gmail.com>
2022-05-30 14:00:48 +0100

    Related Items
    - Changed the article type from model driven to schema
    - Added related items

--- plone-5/instance/src/ukstats.article_type/src/ukstats/article_type/article.py
+++ plone-5/instance/src/ukstats.article_type/src/ukstats/article_type/article.py
@@ -2,7 +2,24 @@
 """Module where all interfaces, events and exceptions live."""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from plone.supermodel import model
+from ukstats.article_type import _
+from z3c.relationfield.schema import RelationChoice
+from z3c.relationfield.schema import RelationList
 
 
 class IUkstatsArticleTypeLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class IArticle(model.Schema):
+    """Schema for Article Type."""
+
+    related_items = RelationList(
+        title="Related Articles",
+        default=[],
+        value_type=RelationChoice(
+            vocabulary="plone.app.vocabularies.Catalog",
+        ),
+        required=False,
+    )
