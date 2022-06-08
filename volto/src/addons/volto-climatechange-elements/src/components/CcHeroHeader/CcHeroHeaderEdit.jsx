2dd0df1614a5710f29a580712bdc3d207d9a9d13
Anoj Perera <anojmperera@gmail.com>
2022-06-08 14:25:14 +0100

    Added form to select the content

--- volto/src/addons/volto-climatechange-elements/src/components/CcHeroHeader/CcHeroHeaderEdit.jsx
+++ volto/src/addons/volto-climatechange-elements/src/components/CcHeroHeader/CcHeroHeaderEdit.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Segment } from 'semantic-ui-react';
-import { SidebarPortal, InlineForm } from '@plone/volto/components';
+import { Segment, Form } from 'semantic-ui-react';
+import { SidebarPortal, Field } from '@plone/volto/components';
 import { DashboardTileSchema } from './schema';
 import { CcHeroHeaderView } from './CcHeroHeaderView';
+import { getRawContent } from '../../actions';
 
 export const CcHeroHeaderEdit = (props) => {
   const { selected, onChangeBlock, block, data } = props;
@@ -12,17 +13,32 @@ export const CcHeroHeaderEdit = (props) => {
     <div>
       <SidebarPortal selected={selected}>
         <Segment.Group raised>
-          <InlineForm
-            schema={schema}
-            title={schema.title}
-            onChangeField={(id, value) => {
-              onChangeBlock(block, {
-                ...data,
-                [id]: value,
-              });
-            }}
-            formData={data}
-          />
+          <Form>
+            <Field
+              id="file_path"
+              widget="object_browser"
+              mode="link"
+              title="Featured Article"
+              widgetOptions={{
+                pattern_options: {
+                  selectableTypes: [],
+                },
+              }}
+              value={data.file_path || []}
+              onChange={(id, value) => {
+                if (value.length > 0) {
+                  const contentId = value[0]?.['@id'];
+                  const path = contentId ? `${contentId}/@@download` : null;
+                  console.log(getRawContent(path));
+                  console.log(data);
+                  onChangeBlock(block, {
+                    ...data,
+                    [id]: value,
+                  });
+                }
+              }}
+            />
+          </Form>
         </Segment.Group>
       </SidebarPortal>
 
