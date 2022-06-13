fa32d754792629f8ffc02554b90b3c696767344c
rossbowen <ross.bowen@ons.gov.uk>
2022-06-13 09:39:53 +0100

    Fix dashboard links

--- volto/src/addons/volto-climatechange-elements/src/customizations/volto/components/theme/Header/Header.jsx
+++ volto/src/addons/volto-climatechange-elements/src/customizations/volto/components/theme/Header/Header.jsx
@@ -32,15 +32,15 @@ const headerConfig = {
                 },
                 {
                     "label": "Impacts",
-                    "href": "/browse/impacts"
+                    "href": "/dashboards/impacts"
                 },
                 {
                     "label": "Mitigation",
-                    "href": "/browse/mitigation"
+                    "href": "/dashboards/mitigation"
                 },
                 {
                     "label": "Adaptation",
-                    "href": "/browse/adaptation"
+                    "href": "/dashboards/adaptation"
                 }
             ],
             "footer_links": [
