08a45158d1878f5cfd5590df5070b932dacded19
Anoj Perera <anojmperera@gmail.com>
2022-06-17 20:01:55 +0100

    Correct Link Added to the Article List

--- volto/src/addons/volto-climatechange-elements/src/components/CcArticleList/CcArticleList.jsx
+++ volto/src/addons/volto-climatechange-elements/src/components/CcArticleList/CcArticleList.jsx
@@ -4,58 +4,23 @@ import { ConditionalLink } from '@plone/volto/components';
 import { flattenToAppURL } from '@plone/volto/helpers';
 
 import { isInternalURL } from '@plone/volto/helpers/Url/Url';
-import { H1, H3, H4 } from 'govuk-react';
+import { H1, H2, H3, H4 } from 'govuk-react';
 
-const CcArticlePreview = ({ data, skipSummary }) => {
+const CcArticlePreview = ({ data, skipSummary, isEditMode }) => {
   return (
     <article className="cc-article-preview">
       <aside className="cc-article-preview--date">{data.publicationDate}</aside>
       <H3>
-        <a href={data.link} class="govuk-link">
+        <a href={data['@id']?.replace('/api', '')} className="govuk-link">
           {data.title}
         </a>
       </H3>
-      {skipSummary ? null : <div>{data.preview}</div>}
+      {skipSummary ? null : <div>{data.description}</div>}
     </article>
   );
 };
 
-const TEST_ARTICLES = [
-  {
-    publicationDate: 'Monday, 1 November 2021',
-    title: 'Emissions embedded in trade and impacts on climate change',
-    preview:
-      'Providing services — including financial, legal and communications services — tends ' +
-      'to emit fewer greenhouse gas emissions than manufacturing goods such as petroleum products, iron, ' +
-      'steel or concrete.',
-    link: '#',
-  },
-  {
-    publicationDate: 'Monday, 1 November 2021',
-    title: 'Measuring greenhouse gas emissions',
-    preview:
-      'The UK is required to report its estimated greenhouse gas (GHG) emissions on a range of' +
-      ' different bases (territorial, residence and footprint) to fulfil a wide range of international ' +
-      'agreements as well as for domestic policy making purposes. The three key official measures of UK ' +
-      'GHG emissions, territorial, residence and footprint, are explored and defined below.',
-    link: '#',
-  },
-  {
-    publicationDate: 'Monday, 1 November 2021',
-    title:
-      'The UK’s climate is changing. What is driving this? How is the UK responding?',
-    preview:
-      'The latest report from the Intergovernmental Panel on Climate Change (IPCC), a United Nations' +
-      ' body providing science-led comprehensive assessments of climate change science, found that climate' +
-      ' change is already happening, with global surface temperatures between 2001 and 2020 around 1°C higher ' +
-      'than during 1850 to 1900, and that this is having effects across the world and in the UK ' +
-      'including making extreme weather events more likely.',
-    link: '#',
-  },
-];
-
 export const CcArticleList = ({ items, linkTitle, linkHref, isEditMode }) => {
-  console.log(items);
   let link = null;
   let href = linkHref?.[0]?.['@id'] || '';
 
@@ -71,16 +36,10 @@ export const CcArticleList = ({ items, linkTitle, linkHref, isEditMode }) => {
 
   return (
     <>
-      <div className="items">
-        {items?.map((item) => (
-          <div className="listing-item" key={item['@id']}>
-            <ConditionalLink item={item} condition={!isEditMode}>
-              <div className="listing-body">
-                <H3>{item.title ? item.title : item.id}</H3>
-                <p>{item.description}</p>
-              </div>
-            </ConditionalLink>
-          </div>
+      <div className="cc-article-list">
+        <H2>Articles</H2>
+        {items?.map((data, i, idx) => (
+          <CcArticlePreview key={i} data={data} />
         ))}
       </div>
 
