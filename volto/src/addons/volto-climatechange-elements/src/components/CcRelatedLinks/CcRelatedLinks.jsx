841d8d37c8c8c5a78ce3fb88b3db33b0368b55fe
Anoj Perera <anojmperera@gmail.com>
2022-06-22 15:37:35 +0100

    Block Variation Changed to Related Links

--- volto/src/addons/volto-climatechange-elements/src/components/CcRelatedLinks/CcRelatedLinks.jsx
+++ volto/src/addons/volto-climatechange-elements/src/components/CcRelatedLinks/CcRelatedLinks.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { H3, H4 } from 'govuk-react';
 
-export const CcRelatedLinks = ({ properties = {}, limit }) => (
+export const CcRelatedLinks = ({ items }) => (
   <div className="cc-related-links">
     <H3>Related Links</H3>
     <ul className="cc-related-links--list">
-      {(properties.relatedItems || []).map((data) => (
+      {(items || []).map((data) => (
         <li key={data['@id']} className="cc-related-links--item">
           <H4>
-            <a href={data['@id']}>{data.title}</a>
+            <a href={data['@id']} className="cc-article-list">
+              {data.title}
+            </a>
           </H4>
-          <div>{data.description}</div>
+          <div className="cc-article-list-description">{data.description}</div>
         </li>
       ))}
     </ul>
