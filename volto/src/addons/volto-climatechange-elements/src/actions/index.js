cdd363000cd3423424af38353161b280820b901c
Anoj Perera <anojmperera@gmail.com>
2022-06-26 08:57:26 +0100

    POST requst added to get related links

--- volto/src/addons/volto-climatechange-elements/src/actions/index.js
+++ volto/src/addons/volto-climatechange-elements/src/actions/index.js
@@ -4,12 +4,29 @@ import {
   GET_FOLDERISH_CONTENT,
 } from '../constants/ActionTypes';
 
-export function getRelatedItemsData(id) {
+export function getRelatedItemsData({
+  b_size = 25,
+  b_start = 0,
+  sort_order = 'descending',
+}) {
   return {
     type: GET_RELATED_ITEMS_DATA,
     request: {
-      op: 'get',
-      path: id,
+      op: 'post',
+      path: '@querystring-search',
+      data: {
+        metadata_fields: '_all',
+        b_size,
+        query: [
+          {
+            i: 'portal_type',
+            o: 'plone.app.querystring.operation.selection.any',
+            v: ['Link'],
+          },
+        ],
+        sort_order,
+        b_start,
+      },
     },
   };
 }
@@ -27,6 +44,7 @@ export function getRawContent(url, headers = {}) {
 }
 
 export function getFolderishContent(url, headers = {}) {
+  console.log(url);
   return {
     type: GET_FOLDERISH_CONTENT,
     request: {
