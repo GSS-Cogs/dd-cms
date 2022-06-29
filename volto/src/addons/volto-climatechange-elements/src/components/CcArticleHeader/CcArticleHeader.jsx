c2852895ad06c716c8a9bedbef7fc6b9854907df
Anoj Perera <anojmperera@gmail.com>
2022-06-29 13:15:00 +0100

    Removed related items from the articles page

--- volto/src/addons/volto-climatechange-elements/src/components/CcArticleHeader/CcArticleHeader.jsx
+++ volto/src/addons/volto-climatechange-elements/src/components/CcArticleHeader/CcArticleHeader.jsx
@@ -4,19 +4,20 @@ import { CcRecentArticles } from '../CcArticleList/CcArticleList';
 import { CcMasthead } from '../CcMasthead/CcMasthead';
 import { FeedSignUps } from '../CcRelatedLinks/FeedSignUps';
 
-export const CcArticleHeader = ({data}) => {
+export const CcArticleHeader = ({ data }) => {
   return (
     <CcMasthead className="app-masthead--article">
       <div className="cc-article-header volto-width-container--wide">
         <div className="govuk-grid-row">
-          <div className="govuk-grid-column-two-thirds govuk-!-padding-right-6">
-            <h1 className="govuk-heading-xl govuk-!-margin-bottom-6">{data.title}</h1>
-            <p className="govuk-caption-m govuk-!-margin-bottom-6">{data.created} by <span className="cc-article-header__date">{data.creators}</span></p>
+          <div className="govuk-!-padding-right-6">
+            <h1 className="govuk-heading-xl govuk-!-margin-bottom-6">
+              {data.title}
+            </h1>
+            <p className="govuk-caption-m govuk-!-margin-bottom-6">
+              {data.created} by{' '}
+              <span className="cc-article-header__date">{data.creators}</span>
+            </p>
             <p className="govuk-body-l">{data.summary}</p>
-          </div>
-          <div className="govuk-grid-column-one-third">
-            <FeedSignUps />
-            <CcRecentArticles articles={data.relatedItems} />
           </div>
         </div>
       </div>
