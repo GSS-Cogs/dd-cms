5dbf7d4fef8ea92d560c8f4b648cc29570990154
Anoj Perera <anojmperera@gmail.com>
2022-06-30 14:54:33 +0100

    Article Header Width Class Changed

--- volto/src/addons/volto-climatechange-elements/src/components/CcArticleHeader/CcArticleHeader.jsx
+++ volto/src/addons/volto-climatechange-elements/src/components/CcArticleHeader/CcArticleHeader.jsx
@@ -7,18 +7,16 @@ import { FeedSignUps } from '../CcRelatedLinks/FeedSignUps';
 export const CcArticleHeader = ({ data }) => {
   return (
     <CcMasthead className="app-masthead--article">
-      <div className="cc-article-header volto-width-container--wide">
-        <div className="govuk-grid-row">
-          <div className="govuk-!-padding-right-6">
-            <h1 className="govuk-heading-xl govuk-!-margin-bottom-6">
-              {data.title}
-            </h1>
-            <p className="govuk-caption-m govuk-!-margin-bottom-6">
-              {data.created} by{' '}
-              <span className="cc-article-header__date">{data.creators}</span>
-            </p>
-            <p className="govuk-body-l">{data.summary}</p>
-          </div>
+      <div className="cc-article-header app-width-container">
+        <div className="govuk-!-padding-right-6">
+          <h1 className="govuk-heading-xl govuk-!-margin-bottom-6">
+            {data.title}
+          </h1>
+          <p className="govuk-caption-m govuk-!-margin-bottom-6">
+            {data.created} by{' '}
+            <span className="cc-article-header__date">{data.creators}</span>
+          </p>
+          <p className="govuk-body-l">{data.summary}</p>
         </div>
       </div>
     </CcMasthead>
