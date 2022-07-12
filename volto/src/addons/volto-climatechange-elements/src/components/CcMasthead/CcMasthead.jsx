f74c00a09266b73c13dd36dfc1817ef580d33ac7
Anoj Perera <anojmperera@gmail.com>
2022-07-12 19:41:12 +0100

    Phase Banner added to Hero Header

--- volto/src/addons/volto-climatechange-elements/src/components/CcMasthead/CcMasthead.jsx
+++ volto/src/addons/volto-climatechange-elements/src/components/CcMasthead/CcMasthead.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { PhaseBanner } from 'govuk-react-jsx';
 
-export const CcMasthead = ({ children, className }) => (
-    <div className={`app-masthead ${className || ''}`}>
-        <div className="app-width-container">
-            <div className="govuk-grid-row">
-                {children}
-            </div>
+export const CcMasthead = ({
+  children,
+  className,
+  shouldDisplayPhaseBanner,
+}) => (
+  <div className={`app-masthead ${className || ''}`}>
+    <div className="app-width-container">
+      {shouldDisplayPhaseBanner ? (
+        <div>
+          <PhaseBanner
+            className="cc-phasebanner-masthead"
+            tag={{
+              children: 'beta',
+            }}
+          >
+            This is a new service your{' '}
+            <Link to="https://example.com">feedback</Link> will help us improve
+            it.
+          </PhaseBanner>
         </div>
+      ) : null}
+      <div className="govuk-grid-row">{children}</div>
     </div>
+  </div>
 );
