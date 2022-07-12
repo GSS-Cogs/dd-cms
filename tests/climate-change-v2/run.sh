8c6795f4a4eb5f7d37f594d030bacab3c66f9ffa
Alex Tucker <alex@floop.org.uk>
2022-07-12 18:38:27 +0100

    Use cucumber-js --exit to try to ensure cucumber exits

--- tests/climate-change-v2/run.sh
+++ tests/climate-change-v2/run.sh
@@ -9,6 +9,6 @@ echo "ready."
 TESTDIR=$PWD
 cd /home/node/app
 export SCREENSHOT_PATH=$TESTDIR/screenshots/
-NODE_PATH=/home/node/app/node_modules ./node_modules/.bin/cucumber-js --require "../**/{cucumber-puppeteer,cucumber-puppeteer-axe}/features/**/*.js" --require "$TESTDIR/features/**/*.js" --world-parameters "{\"executablePath\":\"/usr/bin/chromium-browser\", \"dumpio\": true, \"defaultViewport\": {\"width\": 1512, \"height\": 982}}" $TESTDIR/features --format=json:$TESTDIR/test-results.json
+NODE_PATH=/home/node/app/node_modules ./node_modules/.bin/cucumber-js --require "../**/{cucumber-puppeteer,cucumber-puppeteer-axe}/features/**/*.js" --require "$TESTDIR/features/**/*.js" --world-parameters "{\"executablePath\":\"/usr/bin/chromium-browser\", \"dumpio\": true, \"defaultViewport\": {\"width\": 1512, \"height\": 982}}" $TESTDIR/features --format=json:$TESTDIR/test-results.json --exit
 cd $TESTDIR
 NODE_PATH=/home/node/app/node_modules node toHTML.js
