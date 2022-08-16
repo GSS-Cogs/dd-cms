f810a957d3093ee56414a01267059d943c53c61b
Anoj Perera <anojmperera@gmail.com>
2022-08-16 09:13:05 +0100

    Updated logic to get user from plone

--- volto/src/addons/volto-climatechange-elements/src/components/CcAuthor/CcAuthor.jsx
+++ volto/src/addons/volto-climatechange-elements/src/components/CcAuthor/CcAuthor.jsx
@@ -3,32 +3,38 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getUser } from '@plone/volto/actions';
 
 export const CcAuthor = ({ authors }) => {
+  console.log(authors);
   const dispatch = useDispatch();
   const users = [];
-  const user = useSelector((state) => {
-    if (state.users.get.error && authors) {
-      return authors;
-    } else if (!state.users.get.error) {
-      return state.users.user.fullname;
-    } else {
-      return null;
-    }
-  });
-
-  if (user) {
-    users.push(user);
-  }
+  let user = null;
   useEffect(() => {
-    if (users.length == 0 && authors) {
+    if (authors) {
       authors.split(',').forEach((author) => {
         dispatch(getUser(author));
       });
     }
-  }, [dispatch]);
+  }, [dispatch, user]);
 
-  return (
+  user = useSelector((state) => {
+    if (state.users.get.error) {
+      if (state.users.get.error.status == 404) {
+        // User not found
+        users.push(authors);
+      }
+    } else {
+      if (state.users.get.loaded) {
+        users.push(state.users.user?.fullname);
+      } else {
+        authors?.split(',').forEach((author) => {
+          users.push(author);
+        });
+      }
+    }
+  });
+
+  return users.length > 0 ? (
     <p className="govuk-caption-m govuk-!-margin-bottom-6">
       Written by {users.join(', ')}
     </p>
-  );
+  ) : null;
 };
