4d0d5fcade8b3480e0392cc2a877b49b42458980
Anoj Perera <anojmperera@gmail.com>
2022-08-17 10:04:09 +0100

    Fixed the loading issue with getting User

--- volto/src/addons/volto-climatechange-elements/src/components/CcAuthor/CcAuthor.jsx
+++ volto/src/addons/volto-climatechange-elements/src/components/CcAuthor/CcAuthor.jsx
@@ -1,36 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUser } from '@plone/volto/actions';
 
 export const CcAuthor = ({ authors }) => {
-  console.log(authors);
   const dispatch = useDispatch();
-  const users = [];
+  const [users, setUsers] = useState([]);
   let user = null;
+
+  user = useSelector((state) => {
+    if (state.users.get.error) {
+      if (state.users.get.error.status == 404) {
+        // User not found
+        return authors;
+      } else {
+        return null;
+      }
+    } else {
+      let foundUser = null;
+      authors?.split(',').forEach((author) => {
+        if (state.users.get.loaded && state.users.user.id == author) {
+          foundUser = state.users.user?.fullname;
+        }
+      });
+      return foundUser;
+    }
+  });
+
   useEffect(() => {
     if (authors) {
       authors.split(',').forEach((author) => {
         dispatch(getUser(author));
       });
     }
-  });
+  }, [dispatch]);
 
-  user = useSelector((state) => {
-    if (state.users.get.error) {
-      if (state.users.get.error.status == 404) {
-        // User not found
-        users.push(authors);
-      }
-    } else {
-      authors?.split(',').forEach((author) => {
-        if (state.users.get.loaded && state.users.user.id == author) {
-          users.push(state.users.user?.fullname);
-        } else {
-          users.push(author);
-        }
-      });
+  useEffect(() => {
+    if (user) {
+      const useArr = [user];
+      setUsers(useArr);
     }
-  });
+  }, [user]);
 
   return users.length > 0 ? (
     <p className="govuk-caption-m govuk-!-margin-bottom-6">
