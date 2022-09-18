63e7d781ffbbbe892a4afcdbdfa0e28a65a212cb
Alex Tucker <alex@floop.org.uk>
2022-09-18 16:51:37 +0100

    Add site.cfg, removing from custom.cfg; can be mounted/overridden. Use buildout.environ to pass env vars to buildout. Use init container to build site.

--- plone-5/make-site.sh
+++ plone-5/make-site.sh
@@ -2,6 +2,6 @@
 
 set -e -o pipefail
 
-buildout -c custom.cfg buildout:parts+=plonesite
+buildout -c site.cfg buildout:parts+=plonesite
 find /data -not -user plone -exec chown plone:plone {} \+
 find /plone -not -user plone -exec chown plone:plone {} \+
