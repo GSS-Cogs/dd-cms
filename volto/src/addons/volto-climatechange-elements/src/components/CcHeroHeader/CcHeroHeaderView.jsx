c3eff651d87d46d72a341310117a8d59720f85b8
StRhys <rhys.st.romaine@gsscogs.uk>
2022-09-23 18:05:56 +0100

    component separation - components seperated out into individual components - waits on content to return before displaying components - clean up of useeffects and usestates - components only rerender when needed

--- volto/src/addons/volto-climatechange-elements/src/components/CcHeroHeader/CcHeroHeaderView.jsx
+++ volto/src/addons/volto-climatechange-elements/src/components/CcHeroHeader/CcHeroHeaderView.jsx
@@ -9,77 +9,105 @@ import { flattenToAppURL, isInternalURL } from '@plone/volto/helpers';
 import { getRawContent } from '../../actions';
 import earth from './Earth.svg';
 
-export const CcHeroHeaderView = (props) => {
-  const [summary, setSummary] = useState('');
-  const [title, setTitle] = useState('');
-  const [caption, setCaption] = useState('');
-  const [image, setImage] = useState('');
-  const [callToAction, setCallToAction] = useState('');
-  const [tag, setTag] = useState('');
-  const [marginInset, setMarginInset] = useState(false);
-  const [phaseBannerDisplay, setPhaseBannerDisplay] = useState(false);
-  const [height, setHeight] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
-  const ref = useRef(null);
+const HeroHeaderTextBlock = ({ content, props }) => {
+  useEffect(() => {}, []);
 
-  let articlePath = '#';
+  return (
+    <>
+      {content ? (
+        <>
+          {props?.data?.tag !== '' && props?.data?.tag !== undefined && (
+            <Tag className="govuk-tag--grey app-masthead__tag">
+              {props?.data?.tag}
+            </Tag>
+          )}
+          <h1 className="govuk-heading-xl app-masthead__title">
+            {content ? content?.data?.title : props?.data?.title}
+          </h1>
+          <p className="app-masthead__description">
+            {content?.data?.description}
+          </p>
+        </>
+      ) : (
+        <>
+          {props?.data?.caption !== '' && (
+            <span className="app-masthead__caption">
+              {props?.data?.caption}
+            </span>
+          )}
+          <h1 className="govuk-heading-xl app-masthead__title">
+            {content ? content?.data?.title : props?.data?.title}
+          </h1>
+          <p className="app-masthead__description">{props?.data?.summary}</p>
+        </>
+      )}
+    </>
+  );
+};
 
-  const dispatch = useDispatch();
-
-  if (props.data.file_path && props.data.file_path.length > 0) {
-    articlePath = props.data.file_path[0]['@id'];
+const CallToActionButton = ({ props, callToAction, articlePath }) => {
+  useEffect(() => {}, []);
+  if (callToAction == '' || callToAction == null) {
+    return <div className="app-masthead__start" />;
   }
+  return (
+    <>
+      <Button
+        isStartButton
+        className="govuk-button--secondary app-masthead__start"
+        href={articlePath}
+      >
+        {props?.data?.call_to_action}
+      </Button>
+    </>
+  );
+};
 
-  const request = useSelector((state) => state.rawData?.[articlePath]);
-  const content = request?.data || null;
-
-  useEffect(() => {
-    if (articlePath !== '#') {
-      dispatch(getRawContent(articlePath));
-    }
-  }, [dispatch, articlePath]);
-
-  useEffect(() => {
-    if (content) {
-      setSummary(content.description);
-      setTitle(content.title);
-      if (props.data.call_to_action != '') {
-        setCallToAction(props.data.call_to_action);
-      }
-      if (props.data.tag != '') {
-        setTag(props.data.tag);
-      }
-    } else if (props.data) {
-      setTitle(props.data.title);
-      setSummary(props.data.summary);
-      setCaption(props.data.caption);
-      setCallToAction('');
-    }
-    const image_source = props.data.image_source;
-    if (image_source && image_source.length > 0) {
-      setImage(props.data.image_source[0]['@id']);
-    } else {
-      setImage('');
-    }
-
-    if (props.data.margin == true) {
-      setMarginInset(true);
-    } else {
-      setMarginInset(false);
-    }
-    if (props.data.bannerDisplay === false) {
-      setPhaseBannerDisplay(false);
-    } else {
-      setPhaseBannerDisplay(true);
-    }
-  }, [content, props.data]);
+const InnerMasthead = ({ content, props, articlePath }) => {
+  const [className, setClassName] = useState(
+    'app-masthead__grid-column govuk-grid-column-one-half',
+  );
+  const imgRef = useRef(null);
 
   useEffect(() => {
-    // Handler to call on window resize
-    let heightOffset = phaseBannerDisplay ? 100 : 25;
+    let image_source = props?.data?.image_source;
+    if (!image_source || image_source.length === 0) {
+      // if no image, make article full width
+      setClassName('app-masthead__grid-column govuk-grid-column-full');
+    } else {
+      setClassName('app-masthead__grid-column govuk-grid-column-one-half');
+    }
+  }, [props.data.image_source]);
+
+  return (
+    <div className="govuk-grid-row" ref={imgRef}>
+      <div className={className}>
+        <HeroHeaderTextBlock content={content} props={props} />
+        <CallToActionButton
+          props={props}
+          articlePath={articlePath}
+          callToAction={props?.data?.call_to_action}
+        />
+      </div>
+      <HeroHeaderImage
+        image={props?.data?.image_source[0]?.['@id']}
+        imgRef={imgRef}
+        props={props}
+      />
+    </div>
+  );
+};
+
+const HeroHeaderImage = ({ image, imgRef, props }) => {
+  const [height, setHeight] = useState(700);
+
+  useEffect(() => {
+    let heightOffset = props.data.bannerDisplay ? 100 : 25;
     const handleResize = () => {
       let tempHeight =
-        ref.current === null ? 100 : ref.current.clientHeight + heightOffset;
+        imgRef.current === null
+          ? 100
+          : imgRef.current.clientHeight + heightOffset;
       if (tempHeight >= 700) {
         tempHeight = 700;
       }
@@ -91,97 +119,92 @@ export const CcHeroHeaderView = (props) => {
     handleResize();
     // Remove event listener on cleanup
     return () => window.removeEventListener('resize', handleResize);
-  });
+  }, []);
 
-  const InnerMasthead = () => {
-    let className = 'app-masthead__grid-column govuk-grid';
-    if (image != '') {
-      // if no image, make article full width
-      className += '-column-one-half';
+  if (image == '' || image == undefined) {
+    return (
+      <div className="govuk-grid-column-one-half app-masthead__grid-column"></div>
+    );
+  }
+
+  let marginOffset = props.data.bannerDisplay ? -75 : 0;
+  var tempImg = flattenToAppURL(image) + '/@@images/image';
+
+  return (
+    <div className="govuk-grid-column-one-half app-masthead__grid-column">
+      <img
+        className="app-masthead__image"
+        //src={image}
+        src={tempImg}
+        alt=""
+        role="presentation"
+        style={{
+          position: 'absolute',
+          height: height,
+          width: height + 100,
+          marginTop: marginOffset,
+        }}
+      />
+    </div>
+  );
+};
+
+export const CcHeroHeaderView = (props) => {
+  const [marginInset, setMarginInset] = useState(false);
+  const [phaseBannerDisplay, setPhaseBannerDisplay] = useState(false);
+
+  let articlePath = '#';
+
+  const dispatch = useDispatch();
+
+  if (props.data.file_path && props.data.file_path.length > 0) {
+    articlePath = props.data.file_path[0]['@id'];
+  }
+
+  const request = useSelector((state) => state.rawData?.[articlePath]);
+  const content = request || null;
+
+  useEffect(() => {
+    doDispatch();
+  }, [dispatch, articlePath]);
+
+  const doDispatch = async () => {
+    if (articlePath !== '#') {
+      dispatch(getRawContent(articlePath));
+    }
+  };
+
+  useEffect(() => {
+    const img = new Image();
+    img.src =
+      flattenToAppURL(props.data.image_source[0]['@id']) + '/@@images/image';
+    if (props.data.margin == true) {
+      setMarginInset(true);
     } else {
-      className += '-column-full';
+      setMarginInset(false);
     }
+    // is this needed here?
+    if (props.data.bannerDisplay === false) {
+      setPhaseBannerDisplay(false);
+    } else {
+      setPhaseBannerDisplay(true);
+    }
+  }, [props.data.margin, props.data.bannerDisplay]);
 
-    if (summary === '' || summary === undefined) {
-      return (
-        <div className="govuk-grid-row" style={{ height: 400 }}>
-          <h1 className="govuk-heading-xl app-masthead__title"></h1>
-        </div>
-      );
-    }
-    return (
-      <div className="govuk-grid-row" ref={ref}>
-        <div className={className}>
-          {content
-            ? tag !== '' &&
-              tag !== undefined && (
-                <Tag className="govuk-tag--grey app-masthead__tag">{tag}</Tag>
-              )
-            : caption != '' && (
-                <span className="app-masthead__caption">{caption}</span>
-              )}
-          <h1
-            onLoad={setIsLoading(false)}
-            className="govuk-heading-xl app-masthead__title"
-          >
-            {title}
-          </h1>
-          <p className="app-masthead__description">{summary}</p>
-          <CallToActionButton />
-        </div>
-        <HeroHeaderImage />
-      </div>
-    );
-  };
-
-  const CallToActionButton = () => {
-    if (callToAction == '' || callToAction == null) {
-      return <div className="app-masthead__start" />;
-    }
-    return (
-      <Button
-        isStartButton
-        className="govuk-button--secondary app-masthead__start"
-        href={articlePath}
-      >
-        {callToAction}
-      </Button>
-    );
-  };
-
-  const HeroHeaderImage = () => {
-    if (image == '' || image == undefined || isLoading) {
-      return (
-        <div className="govuk-grid-column-one-half app-masthead__grid-column"></div>
-      );
-    }
-    let marginOffset = phaseBannerDisplay ? -75 : 0;
-    var tempImg = flattenToAppURL(image) + '/@@images/image';
-    return (
-      <div className="govuk-grid-column-one-half app-masthead__grid-column">
-        <img
-          className="app-masthead__image"
-          //src={image}
-          src={tempImg}
-          alt=""
-          role="presentation"
-          style={{
-            position: 'absolute',
-            height: height,
-            width: height + 100,
-            marginTop: marginOffset,
-          }}
-        />
-      </div>
-    );
-  };
+  if (content?.loading || (content === null && articlePath !== '#')) {
+    return <div></div>;
+  }
 
   return (
     <CcMasthead
       className={marginInset && 'app-masthead--bottom-overlap'}
       shouldDisplayPhaseBanner={phaseBannerDisplay}
     >
-      <InnerMasthead />
+      <InnerMasthead
+        content={content}
+        props={props}
+        articlePath={articlePath}
+      />
     </CcMasthead>
   );
 };
