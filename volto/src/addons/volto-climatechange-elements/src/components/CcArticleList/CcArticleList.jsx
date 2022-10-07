ef1e55b5a73f8a9a26b5b9040edf05ee2848f3a9
StRhys <rhys.st.romaine@gsscogs.uk>
2022-10-07 13:41:05 +0100

    spacing between article sections modified - spacing between text sections is now a uniform 25px - following more closely the figma designs of 24px

--- volto/src/addons/volto-climatechange-elements/src/components/CcArticleList/CcArticleList.jsx
+++ volto/src/addons/volto-climatechange-elements/src/components/CcArticleList/CcArticleList.jsx
@@ -11,13 +11,18 @@ export const CcArticlePreview = ({ data, skipSummary, authors }) => {
       <aside className="govuk-caption-m govuk-!-font-weight-bold govuk-!-margin-bottom-2">
         {moment(publishedDate).format('dddd D MMMM YYYY')}
       </aside>
-      <H3>
+      {/* <H3>
         <a href={data['@id']?.replace('/api', '')} className="cc-article-list">
           {data.title}
         </a>
-      </H3>
+      </H3> */}
+      <h3 class="govuk-heading-s govuk-!-margin-bottom-5">
+        <a href={data['@id']?.replace('/api', '')} className="cc-article-list">
+          {data.title}
+        </a>
+      </h3>
       {authors ? (
-        <p className="govuk-caption-m govuk-!-margin-bottom-6">
+        <p className="govuk-caption-m govuk-!-margin-bottom-5">
           Written by {authors}
         </p>
       ) : null}
