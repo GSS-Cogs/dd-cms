34639401223d400a9359b9b06bcfa8613a52082d
StRhys <rhys.st.romaine@gsscogs.uk>
2022-10-19 15:07:37 +0100

    get url from @id

--- volto/src/addons/volto-climatechange-elements/src/customizations/volto/components/theme/Header/Header.jsx
+++ volto/src/addons/volto-climatechange-elements/src/customizations/volto/components/theme/Header/Header.jsx
@@ -75,7 +75,7 @@ const Header = (props) => {
   dashBoardItems.map((item) => {
     menu_contents.push({
       label: item.title,
-      href: `${item.url}`,
+      href: `${item['@id']}`,
       description: item.description,
     });
   });
