10dfa3a14ac63eec8a3e8632e95c3d82c07ad06e
StRhys <rhys.st.romaine@gsscogs.uk>
2022-10-21 16:28:57 +0100

    styling on the default view - margin on top and bottom - two-thirds view - if statement based on  pathname, stop styling effecting landingpage

--- volto/src/addons/volto-govuk-theme/src/customizations/volto/components/theme/View/DefaultView.jsx
+++ volto/src/addons/volto-govuk-theme/src/customizations/volto/components/theme/View/DefaultView.jsx
@@ -3,120 +3,135 @@
  * @module components/theme/View/DefaultView
  */
 
- import React from 'react';
- import PropTypes from 'prop-types';
- import { defineMessages, injectIntl } from 'react-intl';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { defineMessages, injectIntl } from 'react-intl';
 
- import { Container, Image } from 'semantic-ui-react';
- import { map } from 'lodash';
- import config from '@plone/volto/registry';
+import { Container, Image } from 'semantic-ui-react';
+import { map } from 'lodash';
+import config from '@plone/volto/registry';
 
- import {
-   getBlocksFieldname,
-   getBlocksLayoutFieldname,
-   hasBlocksData,
-   getBaseUrl,
- } from '@plone/volto/helpers';
+import {
+  getBlocksFieldname,
+  getBlocksLayoutFieldname,
+  hasBlocksData,
+  getBaseUrl,
+} from '@plone/volto/helpers';
 
- const messages = defineMessages({
-   unknownBlock: {
-     id: 'Unknown Block',
-     defaultMessage: 'Unknown Block {block}',
-   },
- });
+const messages = defineMessages({
+  unknownBlock: {
+    id: 'Unknown Block',
+    defaultMessage: 'Unknown Block {block}',
+  },
+});
 
- /**
-  * Component to display the default view.
-  * @function DefaultView
-  * @param {Object} content Content object.
-  * @returns {string} Markup of the component.
-  */
- const DefaultView = ({ content, intl, location }) => {
-   const blocksFieldname = getBlocksFieldname(content);
-   const blocksLayoutFieldname = getBlocksLayoutFieldname(content);
+/**
+ * Component to display the default view.
+ * @function DefaultView
+ * @param {Object} content Content object.
+ * @returns {string} Markup of the component.
+ */
+const DefaultView = ({ content, intl, location }) => {
+  const blocksFieldname = getBlocksFieldname(content);
+  const blocksLayoutFieldname = getBlocksLayoutFieldname(content);
 
-   return hasBlocksData(content) ? (
-     <div id="page-document">
-       {map(content[blocksLayoutFieldname].items, (block) => {
-         const Block =
-           config.blocks.blocksConfig[
-             content[blocksFieldname]?.[block]?.['@type']
-           ]?.['view'] || null;
-         return Block !== null ? (
-           <Block
-             key={block}
-             id={block}
-             properties={content}
-             data={content[blocksFieldname][block]}
-             path={getBaseUrl(location?.pathname || '')}
-           />
-         ) : (
-           <div key={block}>
-             {intl.formatMessage(messages.unknownBlock, {
-               block: content[blocksFieldname]?.[block]?.['@type'],
-             })}
-           </div>
-         );
-       })}
-     </div>
-   ) : (
-     <Container id="page-document">
-       <h1 className="documentFirstHeading">{content.title}</h1>
-       {content.description && (
-         <p className="documentDescription">{content.description}</p>
-       )}
-       {content.image && (
-         <Image
-           className="document-image"
-           src={content.image.scales.thumb.download}
-           floated="right"
-         />
-       )}
-       {content.remoteUrl && (
-         <span>
-           The link address is:
-           <a href={content.remoteUrl}>{content.remoteUrl}</a>
-         </span>
-       )}
-       {content.text && (
-         <div
-           dangerouslySetInnerHTML={{
-             __html: content.text.data,
-           }}
-         />
-       )}
-     </Container>
-   );
- };
+  const BlockContent = () =>
+    map(content[blocksLayoutFieldname].items, (block) => {
+      const Block =
+        config.blocks.blocksConfig[
+          content[blocksFieldname]?.[block]?.['@type']
+        ]?.['view'] || null;
+      return Block !== null ? (
+        <>
+          <Block
+            key={block}
+            id={block}
+            properties={content}
+            data={content[blocksFieldname][block]}
+            path={getBaseUrl(location?.pathname || '')}
+          />
+        </>
+      ) : (
+        <div key={block}>
+          {intl.formatMessage(messages.unknownBlock, {
+            block: content[blocksFieldname]?.[block]?.['@type'],
+          })}
+        </div>
+      );
+    });
 
- /**
-  * Property types.
-  * @property {Object} propTypes Property types.
-  * @static
-  */
- DefaultView.propTypes = {
-   /**
-    * Content of the object
-    */
-   content: PropTypes.shape({
-     /**
-      * Title of the object
-      */
-     title: PropTypes.string,
-     /**
-      * Description of the object
-      */
-     description: PropTypes.string,
-     /**
-      * Text of the object
-      */
-     text: PropTypes.shape({
-       /**
-        * Data of the text of the object
-        */
-       data: PropTypes.string,
-     }),
-   }).isRequired,
- };
+  return hasBlocksData(content) ? (
+    <div id="page-document">
+      {location.pathname === '/' ? (
+        <BlockContent />
+      ) : (
+        <div className="govuk-cookie-banner__message app-width-container govuk-!-margin-bottom-9 govuk-!-margin-top-6">
+          <div className="govuk-grid-row">
+            <div className="govuk-grid-column-two-thirds">
+              <BlockContent />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  ) : (
+    <Container id="page-document">
+      <h1 className="documentFirstHeading">{content.title}</h1>
+      {content.description && (
+        <p className="documentDescription">{content.description}</p>
+      )}
+      {content.image && (
+        <Image
+          className="document-image"
+          src={content.image.scales.thumb.download}
+          floated="right"
+        />
+      )}
+      {content.remoteUrl && (
+        <span>
+          The link address is:
+          <a href={content.remoteUrl}>{content.remoteUrl}</a>
+        </span>
+      )}
+      {content.text && (
+        <div
+          dangerouslySetInnerHTML={{
+            __html: content.text.data,
+          }}
+        />
+      )}
+    </Container>
+  );
+};
 
- export default injectIntl(DefaultView);
+/**
+ * Property types.
+ * @property {Object} propTypes Property types.
+ * @static
+ */
+DefaultView.propTypes = {
+  /**
+   * Content of the object
+   */
+  content: PropTypes.shape({
+    /**
+     * Title of the object
+     */
+    title: PropTypes.string,
+    /**
+     * Description of the object
+     */
+    description: PropTypes.string,
+    /**
+     * Text of the object
+     */
+    text: PropTypes.shape({
+      /**
+       * Data of the text of the object
+       */
+      data: PropTypes.string,
+    }),
+  }).isRequired,
+};
+
+export default injectIntl(DefaultView);
