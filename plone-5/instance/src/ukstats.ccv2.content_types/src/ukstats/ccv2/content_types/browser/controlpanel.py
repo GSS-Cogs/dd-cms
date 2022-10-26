8b4f3b405a684b3954f607b180dd64cff93d2561
Anoj Perera <anojmperera@gmail.com>
2022-10-26 13:11:52 +0100

    Configured site settings

---
+++ plone-5/instance/src/ukstats.ccv2.content_types/src/ukstats/ccv2/content_types/browser/controlpanel.py
@@ -0,0 +1,62 @@
+from plone import schema
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.autoform import directives
+from plone.restapi.controlpanels import RegistryConfigletPanel
+from zope.component import adapter
+from zope.interface import Interface
+
+
+class ICMSSiteSettings(Interface):
+    site_title = schema.Textline(
+        title="Site Title",
+        description="Set site title",
+        default="Fantastic CMS Site",
+        required=False,
+    )
+
+    hotjar_analytics_id = schema.Textline(
+        title="Hotjar Analytics ID",
+        description="Set hotjar analytics ID",
+        default="",
+        required=False,
+    )
+
+    google_analytics_id = schema.Textline(
+        title="Google Analytics ID",
+        description="Set google analytics ID",
+        default="",
+        required=False,
+    )
+
+    directives.widget("site_title", frontendOptions={"widget": "TextLinesFieldWidget"})
+
+    directives.widget(
+        "hotjar_analytics_id", frontendOptions={"widget": "TextLinesFieldWidget"}
+    )
+
+    directives.widget(
+        "google_analytics_id", frontendOptions={"widget": "TextLinesFieldWidget"}
+    )
+
+
+class CMSconfRegistryEditForm(RegistryEditForm):
+    schema = ICMSSiteSettings
+    schema_prefix = "cmsconf"
+    label = "CMS Settings"
+
+
+class CMSControlPanelFormWrapper(ControlPanelFormWrapper):
+    form = CMSconfRegistryEditForm
+
+
+@adapter(Interface, Interface)
+class CMSRegistryConfigletPanel(RegistryConfigletPanel):
+    """Volto control panel"""
+
+    schema = ICMSSiteSettings
+    schema_prefix = "cmsconf"
+    configlet_id = "cmsconf-controlpanel"
+    configlet_category_id = "CMS"
+    title = "CMS Settings"
+    group = "CMS"
