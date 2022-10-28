5642da00d2101c78d7a8276480a21e0d4c6e080c
StRhys <rhys.st.romaine@gsscogs.uk>
2022-10-28 12:54:56 +0100

    create CcV2ArticleWithToCView from template

---
+++ volto/src/addons/volto-climatechange-elements/src/components/CcV2Preview/CcV2ArticleWithToCView.jsx
@@ -0,0 +1,62 @@
+/**
+ * article with toc view component.
+ * @module components/theme/View/CcV2ArticleWithToCView
+ */
+
+import React from 'react';
+import { map } from 'lodash';
+import config from '@plone/volto/registry';
+import {
+  getBlocksFieldname,
+  getBlocksLayoutFieldname,
+  getBaseUrl,
+} from '@plone/volto/helpers';
+
+import { CcArticleHeader } from '../CcArticleHeader/CcArticleHeader';
+import { formattedDate } from '../../utils';
+
+export const CcV2ArticleWithToCView = (props) => {
+  const { content, location } = props;
+  const blocksFieldname = getBlocksFieldname(content);
+  const blocksLayoutFieldname = getBlocksLayoutFieldname(content);
+
+  const formattedCreators = (creators) => creators.join(', ');
+  return (
+    <div>
+      <CcArticleHeader
+        data={{
+          title: content.title,
+          summary: content.description,
+          created: formattedDate(content.effective ?? content.created),
+          creators: formattedCreators(content.creators),
+          dashboard: content?.['@id'].includes('dashboard'),
+        }}
+      />
+      <div className="volto-width-container--wide ccv2-article-body">
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-two-thirds">
+            {map(content[blocksLayoutFieldname].items, (block) => {
+              const Block =
+                config.blocks.blocksConfig[
+                  content[blocksFieldname]?.[block]?.['@type']
+                ]?.['view'] || null;
+
+              const notTitleBlock =
+                content[blocksFieldname]?.[block]?.['@type'] !== 'title';
+
+              return Block !== null && notTitleBlock ? (
+                <Block
+                  key={block}
+                  id={block}
+                  properties={content}
+                  data={content[blocksFieldname][block]}
+                  path={getBaseUrl(location?.pathname || '')}
+                />
+              ) : null;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
