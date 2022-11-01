a15da869fdd09c0438481ecdbd1c8bccb4571b7a
StRhys <rhys.st.romaine@gsscogs.uk>
2022-11-01 16:48:52 +0000

    table of content - gets toc content from content[blocksLayoutFieldname].items - cleanse data to headers - display toc on left, one-third width - get screen width with event handler - changed height of toc based on screen width - display toc at top on mobile width - display 'back to content' mobile width

--- volto/src/addons/volto-climatechange-elements/src/components/CcV2Preview/CcV2ArticleWithToCView.jsx
+++ volto/src/addons/volto-climatechange-elements/src/components/CcV2Preview/CcV2ArticleWithToCView.jsx
@@ -3,7 +3,7 @@
  * @module components/theme/View/CcV2ArticleWithToCView
  */
 
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { map } from 'lodash';
 import config from '@plone/volto/registry';
 import {
@@ -17,10 +17,76 @@ import { formattedDate } from '../../utils';
 
 export const CcV2ArticleWithToCView = (props) => {
   const { content, location } = props;
+
   const blocksFieldname = getBlocksFieldname(content);
   const blocksLayoutFieldname = getBlocksLayoutFieldname(content);
+  const mainContentRef = useRef(null);
 
   const formattedCreators = (creators) => creators.join(', ');
+
+  const [contentHeaders, setContentHeaders] = useState([]);
+  const [screenWidth, setScreenWidth] = useState(802);
+
+  useEffect(() => {
+    // Handler to call on window resize
+    const handleResize = () => {
+      let width = window.innerWidth;
+      setScreenWidth(width);
+    };
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, [screenWidth]);
+
+  useEffect(() => {
+    let tempHeaders = [];
+    let currentIndex = -1;
+    content[blocksLayoutFieldname].items.forEach((block) => {
+      const contentBlock = content[blocksFieldname][block];
+      if (blocksFieldname && contentBlock?.value !== undefined) {
+        if (contentBlock?.value['0']?.type === 'h4') {
+          //console.log(contentBlock.plaintext);
+          const text = contentBlock?.plaintext;
+
+          tempHeaders[currentIndex]['sub'].push({
+            id: block,
+            text: text.trim(),
+          });
+        } else if (
+          contentBlock?.value['0']?.type !== 'h2' &&
+          contentBlock?.plaintext.length > 2 &&
+          contentBlock?.plaintext.length < 40 &&
+          contentBlock?.plaintext.charAt(0) === ' '
+        ) {
+          // console.log(contentBlock?.plaintext);
+          // console.log(contentBlock);
+          const text = contentBlock?.plaintext;
+
+          tempHeaders[currentIndex]['sub'].push({
+            id: block,
+            text: text.trim(),
+          });
+        } else if (
+          contentBlock?.value['0']?.type === 'h2' ||
+          contentBlock?.value['0']?.type === 'h3'
+        ) {
+          //console.log(contentBlock.plaintext);
+          const text = contentBlock?.plaintext;
+
+          tempHeaders.push({ id: block, text: text.trim(), sub: [] });
+          currentIndex += 1;
+        }
+      }
+    });
+    console.log(tempHeaders);
+    setContentHeaders(tempHeaders);
+  }, []);
+
+  let previousType = '';
+
   return (
     <div>
       <CcArticleHeader
@@ -32,9 +98,55 @@ export const CcV2ArticleWithToCView = (props) => {
           dashboard: content?.['@id'].includes('dashboard'),
         }}
       />
-      <div className="volto-width-container--wide ccv2-article-body">
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-two-thirds">
+      <div
+        className="volto-width-container--wide ccv2-article-body"
+        id="navigation"
+      >
+        <div
+          className="govuk-grid-row ccv2-article-body--main"
+          style={{
+            //backgroundColor: 'lightgreen',
+            height:
+              screenWidth <= 801
+                ? '100%'
+                : mainContentRef.current === null
+                ? 1000
+                : mainContentRef.current.clientHeight,
+          }}
+        >
+          <div className="govuk-grid-column-one-third ccv2-article-nav-container">
+            <nav className="ccv2-article-nav">
+              <h3 className="ccv2-article-nav--title">Contents</h3>
+              <ul className="govuk-list">
+                {contentHeaders.length > 0 &&
+                  contentHeaders.map((item, index) => {
+                    return (
+                      <li>
+                        <a className="govuk-link" href={'#' + item.id}>
+                          {item.text}
+                        </a>
+                        <ul className="govuk-list">
+                          {contentHeaders[index].sub.length > 0 &&
+                            contentHeaders[index].sub.map((subitem) => {
+                              return (
+                                <li>
+                                  <a
+                                    className="govuk-link"
+                                    href={'#' + subitem.id}
+                                  >
+                                    {subitem.text}
+                                  </a>
+                                </li>
+                              );
+                            })}
+                        </ul>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </nav>
+          </div>
+          <div className="govuk-grid-column-two-thirds" ref={mainContentRef}>
             {map(content[blocksLayoutFieldname].items, (block) => {
               const Block =
                 config.blocks.blocksConfig[
@@ -43,15 +155,46 @@ export const CcV2ArticleWithToCView = (props) => {
 
               const notTitleBlock =
                 content[blocksFieldname]?.[block]?.['@type'] !== 'title';
+              let displayBack = false;
+              const contentBlock = content[blocksFieldname][block];
+              if (contentBlock?.value === undefined) {
+                displayBack = true;
+              }
+              // if (
+              //   Block !== null &&
+              //   notTitleBlock &&
+              //   contentBlock?.value !== undefined
+              // ) {
+              //   if (
+              //     previousType === 'p' &&
+              //     contentBlock?.value['0']?.type !== previousType
+              //   ) {
+              //     displayBack = true;
+              //   }
+              //   console.log(contentBlock?.value);
+              //   previousType = contentBlock?.value['0']?.type;
+              // }
 
               return Block !== null && notTitleBlock ? (
-                <Block
-                  key={block}
-                  id={block}
-                  properties={content}
-                  data={content[blocksFieldname][block]}
-                  path={getBaseUrl(location?.pathname || '')}
-                />
+                <>
+                  <div id={block}>
+                    {displayBack && screenWidth <= 801 && (
+                      <a
+                        className="govuk-body-m govuk-link"
+                        href={'#navigation'}
+                      >
+                        back to contents
+                      </a>
+                    )}
+                    <Block
+                      key={block}
+                      id={block}
+                      properties={content}
+                      data={content[blocksFieldname][block]}
+                      path={getBaseUrl(location?.pathname || '')}
+                    />
+                  </div>
+                </>
               ) : null;
             })}
           </div>
