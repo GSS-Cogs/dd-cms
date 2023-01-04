356f756f6ee7f0cbc9e4bf22e6b318a580b929bd
Anoj Perera <anojmperera@gmail.com>
2023-01-04 11:00:26 +0000

    Fixed feature and step definitions for bdd test

--- tests/climate-change-v2/features/step-definitions/browser.js
+++ tests/climate-change-v2/features/step-definitions/browser.js
@@ -1,61 +1,74 @@
-const { When } = require('cucumber');
-const {strict: assert} = require("assert");
+const { When, Given } = require('cucumber');
+const { strict: assert } = require("assert");
+
+When('I login using {string} and wait for the element {string}', async function(selector, waitForSelector) {
+  const url = 'http://localhost:3000/login?return_url=';
+
+  await Promise.all([
+    this.page.waitForSelector(waitForSelector),
+    this.page.goto(url),
+  ]);
+});
 
 When(
-    'I click the xpath link {string}', async function(xpath) {
-        await this.page.$x(xpath).then(links => links[0].click())
-    }
+  'I click the xpath link {string}', async function(xpath) {
+    await this.page.$x(xpath).then(links => links[0].click())
+  }
 );
 
 When(
-    'I click the xpath link {string} and wait for the network to be idle', async function(xpath) {
-        await Promise.all([
-            this.page.waitForNavigation({waitUntil: "networkidle0"}),
-            this.page.$x(xpath).then(links => links[0].click())
-        ]);
-    }
+  'I click the xpath link {string} and wait for the network to be idle', async function(xpath) {
+    await Promise.all([
+      this.page.waitForNavigation({ waitUntil: "networkidle0" }),
+      this.page.$x(xpath).then(links => links[0].click())
+    ]);
+  }
 );
 
 When(
-    'I reload the page', async function() {
-        await this.page.reload({waitUntil: ["load"]});
-    }
+  'I reload the page', async function() {
+    await this.page.reload({ waitUntil: ["load"] });
+  }
 );
 
 When(
-    'I wait for navigation to complete', async function() {
-        await this.page.waitForNavigation();
-    }
+  'I wait for navigation to complete', async function() {
+    await this.page.waitForNavigation();
+  }
 );
 
 // This is only available from Puppeteer >= v10.4.0
 When(
-    'I wait for the network to be idle', async function() {
-        await this.page.waitForNetworkIdle();
-    }
+  'I wait for the network to be idle', async function() {
+    await this.page.waitForNetworkIdle();
+  }
 )
 
 When(
-    'I type {string} in the {string} element', async function(text, element) {
-        await this.page.focus(element);
-        await this.page.keyboard.type(text);
-    }
+  'I type {string} in the {string} element', async function(text, element) {
+    await this.page.focus(element);
+    await this.page.keyboard.type(text);
+  }
 )
 
 When(
-    'I wait for xpath {string} to be visible', async function(xpath) {
-        await this.page.waitForXPath(xpath, {visible: true});
-    }
+  'I wait for xpath {string} to be visible', async function(xpath) {
+  await this.page.waitForXPath(xpath, { visible: true });
+  }
 )
 
 When(
-    'I click the {string} element containing {string}', async function(element, text) {
-        await this.page.$x(`//${element}[contains(text(), '${text}')]`).then(nodes => nodes[0].click())
+  'I click the {string} element containing {string}', async function(element, text) {
+    if (text.contains("login")) {
+      await this.page.load('/login?return_url=');
+    } else {
+      await this.page.$x(`//${element}[contains(text(), '${text}')]`).then(nodes => nodes[0].click())
     }
+  }
 )
 
 When(
-    'the image {string} should decode ok', async function(selector) {
-        await this.page.$eval(selector, (img => img.decode()));
-    }
+  'the image {string} should decode ok', async function(selector) {
+    await this.page.$eval(selector, (img => img.decode()));
+  }
 )
