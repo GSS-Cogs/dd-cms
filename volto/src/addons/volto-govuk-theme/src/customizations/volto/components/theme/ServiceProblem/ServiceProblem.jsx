0b01cf109a06b43ab40f0903d170b4c18df30a7e
Rhys St Romaine <rhys.st.romaine@ons.gov.uk>
2023-02-03 10:11:12 +0000

    replace lorem ipsum

--- volto/src/addons/volto-govuk-theme/src/customizations/volto/components/theme/ServiceProblem/ServiceProblem.jsx
+++ volto/src/addons/volto-govuk-theme/src/customizations/volto/components/theme/ServiceProblem/ServiceProblem.jsx
@@ -24,10 +24,8 @@ const ServiceProblem = () => (
             Sorry, there is a problem with the service
           </h1>
           <p class="govuk-body">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            Our Support Team is working on the error, and will resolve it as
+            soon as possible.
           </p>
           <p class="govuk-body">Try again later.</p>
         </div>
