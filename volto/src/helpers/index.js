91e0c7373d5d4f3289abca1acb1bed110b1e8c08
Rhys St Romaine <rhys.st.romaine@ons.gov.uk>
2023-02-24 12:41:42 +0000

    helper function to replace url

--- volto/src/helpers/index.js
+++ volto/src/helpers/index.js
@@ -4,3 +4,14 @@
  * @example
  * export { Api } from './Api/Api';
  */
+
+const replaceUrl = (originalPath, parentPath) => {
+  const dashboard_name = parentPath.match(/(?<=\/dashboards\/).*./);
+  const possible_url = originalPath.replace(
+    /(?<=\/dashboards\/).+?(?=\/)/,
+    dashboard_name,
+  );
+  return possible_url;
+};
+
+export { replaceUrl };
