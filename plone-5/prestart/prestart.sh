98c6f64a96e8a043e859b1ec15954f0933534a03
mikeAdamss <mikelovesbooks@gmail.com>
2023-03-14 09:02:40 +0000

    wip

--- plone-5/prestart/prestart.sh
+++ plone-5/prestart/prestart.sh
@@ -4,5 +4,14 @@
 # If using python - don't drag in new dependencies, standard library only please.
 # If using bash - remember to chmod it.
 
+# Explcitly set standard environment variables
+# TODO - I don't think these should be missing, I think this is
+# a sign of some sort of misconfiguration, figure it out.
+export PLONE_INSTANCE=/plone/instance/bin/instance
+export PLONE_HOME=/plone/instance
+
+# Patching
+patch /plone/buildout-cache/eggs/cp38/Zope-4.6.3-py3.8.egg/OFS/ObjectManager.py objectmanager.patch
+
 # Inject env vars into the zope.conf.
 python3 /plone/instance/prestart/configure_db_from_env.py
