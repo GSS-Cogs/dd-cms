2037e1d2bed28593e770b997652617016fdcfdd1
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2023-08-02 15:11:03 +0100

    made requested changes

--- plone-5/instance/src/ukstats.ccv2.content_types/src/ukstats/ccv2/content_types/api/controlpanel.py
+++ plone-5/instance/src/ukstats.ccv2.content_types/src/ukstats/ccv2/content_types/api/controlpanel.py
@@ -3,12 +3,12 @@ from plone import api
 from plone.restapi.services import Service
 from plone.restapi.serializer.converters import json_compatible
 
-reg_base = 'cmsconf'
+REG_BASE = 'cmsconf'
 
 
 class SiteTitle(Service):
     def reply(self):
-        registry_record_value = api.portal.get_registry_record(reg_base + '.site_title')
+        registry_record_value = api.portal.get_registry_record(REG_BASE + '.site_title')
         response_data = {
             'site_title': registry_record_value
         }
@@ -18,18 +18,18 @@ class SiteTitle(Service):
 class PhaseBanner(Service):
     def reply(self):
         phasebanner_state = api.portal.get_registry_record(
-            reg_base + '.phasebanner_state')
-        feedback_email = api.portal.get_registry_record(reg_base + '.feedback_email')
+            REG_BASE + '.phasebanner_state')
+        feedback_email = api.portal.get_registry_record(REG_BASE + '.feedback_email')
         return {'bannerDisplay': phasebanner_state, 'bannerLink': feedback_email}
 
-class GA_ID(Service):
+class GAID(Service):
     def reply(self):
-        ga_id_value = api.portal.get_registry_record(reg_base + '.google_analytics_id')
+        ga_id_value = api.portal.get_registry_record(REG_BASE + '.google_analytics_id')
         response_value = {'google_analytics_id': ga_id_value}
         return json_compatible(response_value)
 
-class Hotjar_ID(Service):
+class HotjarID(Service):
     def reply(self):
-        hotjar_id_value = api.portal.get_registry_record(reg_base + '.hotjar_analytics_id')
+        hotjar_id_value = api.portal.get_registry_record(REG_BASE + '.hotjar_analytics_id')
         response_value = {'hotjar_analytics_id' : hotjar_id_value}
         return json_compatible(response_value)
