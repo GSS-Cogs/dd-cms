0b106ad75815b53d3b4542873da4e00b963ea4f1
Rhys St Romaine <rhys.st.romaine@ons.gov.uk>
2023-10-30 14:33:25 +0000

    update links

--- volto/src/addons/volto-climatechange-elements/src/customizations/volto/components/theme/ClimateChangeNotification/ClimateChangeNotification.jsx
+++ volto/src/addons/volto-climatechange-elements/src/customizations/volto/components/theme/ClimateChangeNotification/ClimateChangeNotification.jsx
@@ -45,7 +45,7 @@ export const ClimateChangeNotification = (props) => {
                 className={`govuk-notification-banner__link cc-notification__link ${
                   'cc-notification__link' + altColor
                 }`}
-                href="https://eur03.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.gov.uk%2Fsearch%2Fresearch-and-statistics%3Fcontent_store_document_type%3Dall_research_and_statistics%26level_one_taxon%3D3cf97f69-84de-41ae-bc7b-7e2cc238fa58%26level_two_taxon%3D62e08e6f-9161-42c6-b9dd-0cf8fcab2c69%26order%3Drelease-date-oldest&data=05%7C01%7Cangela.watkins%40ons.gov.uk%7C080f81b3182747004c8708dbcf2f6358%7C078807bfce824688bce00d811684dc46%7C0%7C0%7C638331855223012354%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C3000%7C%7C%7C&sdata=Vvfv3bWD3JiNd6vaoujEOaxneVC4HGWvi2lE9whJtW4%3D&reserved=0"
+                href="https://www.gov.uk/search/research-and-statistics?content_store_document_type=all_research_and_statistics&level_one_taxon=3cf97f69-84de-41ae-bc7b-7e2cc238fa58&level_two_taxon=62e08e6f-9161-42c6-b9dd-0cf8fcab2c69&order=release-date-oldest"
               >
                 gov.uk
               </a>{' '}
@@ -63,7 +63,7 @@ export const ClimateChangeNotification = (props) => {
                 className={`govuk-notification-banner__link cc-notification__link ${
                   'cc-notification__link' + altColor
                 }`}
-                href="https://eur03.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.gov.scot%2Fstatistics-and-research%2F&data=05%7C01%7Cangela.watkins%40ons.gov.uk%7C080f81b3182747004c8708dbcf2f6358%7C078807bfce824688bce00d811684dc46%7C0%7C0%7C638331855223012354%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C3000%7C%7C%7C&sdata=0ZOZEFgSkShqIZeStkq%2BYOl3SYz61OsTzeQI5tO957I%3D&reserved=0"
+                href="https://www.gov.scot/statistics-and-research/"
               >
                 Scottish Government
               </a>{' '}
